38988dd38f97e231d3b5511a8f8197a28cafac2e
Tefa95100 <damien.julien.95100@gmail.com>
2025-10-07 23:23:23 +0200

    Add endpoints health and version

--- services/api/app/urls.py
+++ services/api/app/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from . import monitoring
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("health", monitoring.health, name="health"),
+    path("version", monitoring.version, name="version")
 ]
