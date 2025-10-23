c0699785bc53c99ff89ea4b398404a50d5dfeccb
Tefa95100 <damien.julien.95100@gmail.com>
2025-10-24 01:59:30 +0200

    Add serializers, urls for api

---
+++ services/api/core/api/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+
+
+class ArticleCreateIn(serializers.Serializer):
+    title = serializers.CharField(min_length=3, max_length=300)
+    url = serializers.URLField()
+    theme = serializers.CharField(allow_null=True, allow_blank=True, required=False)
+
+
+class ArticleUpdateIn(serializers.Serializer):
+    title = serializers.CharField(min_length=3, max_length=300, required=False)
+    url = serializers.URLField(required=False)
+    theme = serializers.CharField(allow_null=True, allow_blank=True, required=False)
+
+
+class ArticleOut(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField()
+    url = serializers.URLField()
+    theme = serializers.CharField(allow_null=True, allow_blank=True, required=False)
