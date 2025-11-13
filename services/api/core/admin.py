54f86caacd4e83d84d29b326bfe796d8e8720ef9
Tefa95100 <damien.julien.95100@gmail.com>
2025-11-13 01:56:37 +0100

    Add interface for add best practices

--- services/api/core/admin.py
+++ services/api/core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from .models import BestPractice, Favorite
 
 from .models import FeedSource, InterestCenter
 
@@ -16,3 +17,15 @@ class InterestCenterAdmin(admin.ModelAdmin):
 @admin.register(FeedSource)
 class FeedSourceAdmin(admin.ModelAdmin):
     list_display = ("url", "last_fetched_at")
+
+
+@admin.register(BestPractice)
+class BestPracticeAdmin(admin.ModelAdmin):
+    list_display = ("title", "created_at")
+    search_fields = ("title",)
+
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ("user", "content_type", "object_id", "created_at")
+    list_filter = ("content_type",)
